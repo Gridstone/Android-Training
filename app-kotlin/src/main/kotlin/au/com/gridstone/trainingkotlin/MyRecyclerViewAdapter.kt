ddba7e59e87dfdc80a34c3e7e04aa8d37112004f
Campbell Graham <campbell_graham@outlook.com>
2019-03-28 16:39:39 +1100

    UI updates + test image

--- app-kotlin/src/main/kotlin/au/com/gridstone/trainingkotlin/MyRecyclerViewAdapter.kt
+++ app-kotlin/src/main/kotlin/au/com/gridstone/trainingkotlin/MyRecyclerViewAdapter.kt
@@ -1,9 +1,13 @@
 package au.com.gridstone.trainingkotlin
 
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+
 
 class MyRecyclerViewAdapter(private val myDataset: Array<String>) :
         RecyclerView.Adapter<MyRecyclerViewAdapter.MyViewHolder>() {
@@ -12,7 +16,7 @@ class MyRecyclerViewAdapter(private val myDataset: Array<String>) :
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class MyViewHolder(val view: FrameLayout) : RecyclerView.ViewHolder(view)
 
 
     // Create new views (invoked by the layout manager)
@@ -20,8 +24,9 @@ class MyRecyclerViewAdapter(private val myDataset: Array<String>) :
                                     viewType: Int): MyRecyclerViewAdapter.MyViewHolder {
         // create a new view
         val textView = LayoutInflater.from(parent.context)
-                .inflate(R.layout.recyclerview_row, parent, false) as TextView
+                .inflate(R.layout.recyclerview_row, parent, false) as FrameLayout
         // set the view's size, margins, paddings and layout parameters
+        textView.setPadding(0,0,0,4)
 
         return MyViewHolder(textView)
     }
@@ -30,7 +35,12 @@ class MyRecyclerViewAdapter(private val myDataset: Array<String>) :
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.text = myDataset[position]
+        var textView = holder.view.findViewById<TextView>(R.id.imageListTitle)
+        textView.text = myDataset[position]
+        textView.background.alpha = 200
+        textView.setGravity(Gravity.CENTER_VERTICAL);
+        var imageView = holder.view.findViewById<ImageView>(R.id.imageListImageView)
+        imageView.adjustViewBounds = true
     }
 
     // Return the size of your dataset (invoked by the layout manager)
