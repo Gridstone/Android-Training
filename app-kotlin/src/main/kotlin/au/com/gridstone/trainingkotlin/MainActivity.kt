36f391589f853f99a31c3d6028ae259ac62d4516
Campbell Graham <campbell_graham@outlook.com>
2019-03-28 11:56:44 +1100

    Basic set up for recyclerView

--- app-kotlin/src/main/kotlin/au/com/gridstone/trainingkotlin/MainActivity.kt
+++ app-kotlin/src/main/kotlin/au/com/gridstone/trainingkotlin/MainActivity.kt
@@ -2,9 +2,19 @@ package au.com.gridstone.trainingkotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var linearLayoutManager: LinearLayoutManager
+
   override fun onCreate(savedInstanceState: Bundle?) {
+
+    linearLayoutManager = LinearLayoutManager(this)
+    var recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view)
+    recyclerView.layoutManager = linearLayoutManager
+
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
