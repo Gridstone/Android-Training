542d9b264ec19e5521de49f0cd7937448cd9b665
Campbell Graham <campbell_graham@outlook.com>
2019-03-28 13:45:40 +1100

    Basic labels within recyclerView rows

--- app-kotlin/src/main/kotlin/au/com/gridstone/trainingkotlin/MainActivity.kt
+++ app-kotlin/src/main/kotlin/au/com/gridstone/trainingkotlin/MainActivity.kt
@@ -7,15 +7,31 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var linearLayoutManager: LinearLayoutManager
+  private lateinit var recyclerView: RecyclerView
+  private lateinit var viewAdapter: RecyclerView.Adapter<*>
+  private lateinit var viewManager: RecyclerView.LayoutManager
 
   override fun onCreate(savedInstanceState: Bundle?) {
 
-    linearLayoutManager = LinearLayoutManager(this)
-    var recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view)
-    recyclerView.layoutManager = linearLayoutManager
-
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    viewManager = LinearLayoutManager(this)
+    viewAdapter = MyRecyclerViewAdapter(
+            arrayOf("1", "2", "3","1", "2", "3","1", "2", "3","1", "2", "3","1", "2", "3","1", "2", "3","1", "2", "3","1", "2", "3","1", "2", "3")
+    )
+
+    recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+      // use this setting to improve performance if you know that changes
+      // in content do not change the layout size of the RecyclerView
+      setHasFixedSize(true)
+
+      // use a linear layout manager
+      layoutManager = viewManager
+
+      // specify an viewAdapter (see also next example)
+      adapter = viewAdapter
+
+    }
   }
 }
