860bfbaa78568f9ab89bedeecd27c78df2dbf0bb
Campbell Graham <campbell_graham@outlook.com>
2019-04-04 11:02:27 +1100

    Load titles + loading state

---
+++ app-kotlin/src/main/kotlin/au/com/gridstone/trainingkotlin/APIManager.kt
@@ -0,0 +1,50 @@
+package au.com.gridstone.trainingkotlin
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.Retrofit
+import com.google.gson.annotations.SerializedName
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Headers
+
+class ImageData {
+    @SerializedName("title")
+    private var title: String? = null
+
+    fun setTitle(title: String) {
+        this.title = title
+    }
+
+    fun getTitle(): String {
+       return title ?: "-"
+    }
+}
+
+class ImageDataResponse {
+    @SerializedName("data")
+    private var data: List<ImageData>? = null
+
+    fun setData(data: List<ImageData>) {
+        this.data = data
+    }
+
+    fun getData(): List<ImageData> {
+        return data ?: ArrayList()
+    }
+}
+
+interface ImgurService {
+    @Headers("Authorization: Client-ID 3436c108ccc17d3")
+    @GET("3/gallery/hot/viral")
+    fun getImages(): Call<ImageDataResponse>
+}
+
+object APIManager {
+
+    private val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.imgur.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    val service = retrofit.create<ImgurService>(ImgurService::class.java)
+}
--- app-kotlin/src/main/kotlin/au/com/gridstone/trainingkotlin/MainActivity.kt
+++ app-kotlin/src/main/kotlin/au/com/gridstone/trainingkotlin/MainActivity.kt
@@ -4,39 +4,58 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var recyclerView: RecyclerView
-  private lateinit var viewAdapter: RecyclerView.Adapter<*>
-  private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
-  override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
 
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
 
-    viewManager = LinearLayoutManager(this)
+        setContentView(R.layout.activity_loading_main)
 
-    val values : MutableList<String> = ArrayList()
+        val values: MutableList<String> = ArrayList()
 
-    for (i in 0..99) {
-      values.add(i.toString())
+        val call = APIManager.service.getImages()
+
+        call.enqueue(object : Callback<ImageDataResponse> {
+            override fun onResponse(call: Call<ImageDataResponse>, response: Response<ImageDataResponse>) {
+                response.body()?.getData()?.forEach {imageData ->
+                    values.add(imageData.getTitle())
+                }
+                populateView(values)
+            }
+
+            override fun onFailure(call: Call<ImageDataResponse>, t: Throwable) {
+                print(t.message)
+            }
+        })
     }
 
-    viewAdapter = MyRecyclerViewAdapter(values.toTypedArray(), this)
+    fun populateView(data: List<String>) {
+        setContentView(R.layout.activity_main)
 
-    recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
-      // use this setting to improve performance if you know that changes
-      // in content do not change the layout size of the RecyclerView
-      setHasFixedSize(true)
+        viewManager = LinearLayoutManager(this)
 
-      // use a linear layout manager
-      layoutManager = viewManager
+        viewAdapter = MyRecyclerViewAdapter(data.toTypedArray(), this)
 
-      // specify an viewAdapter (see also next example)
-      adapter = viewAdapter
+        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
 
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+
+        }
     }
-  }
 }
