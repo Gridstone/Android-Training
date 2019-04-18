add4b073106a4e4574099af1cc7474bd3e44f07b
Campbell Graham <campbell_graham@outlook.com>
2019-04-18 11:13:40 +1000

    Refactor pokemon displayable

--- app-kotlin/src/main/kotlin/au/com/gridstone/trainingkotlin/PokemonDisplayable.kt
+++ app-kotlin/src/main/kotlin/au/com/gridstone/trainingkotlin/PokemonDisplayable.kt
@@ -1,50 +1,37 @@
 package au.com.gridstone.trainingkotlin
 
 
-class PokemonDisplayable(val pokemon: Pokemon) {
-
-  val title: String
-    get() = pokemon.name.capitalize()
-
-  val imageURL: String
-    get() {
-      val formattedID = String.format("%03d", pokemon.id)
-      return "https://assets.pokemon.com/assets/cms2/img/pokedex/full/$formattedID.png"
-    }
+class PokemonDisplayable(pokemon: Pokemon) {
 
   val attackValue: String
-    get() {
-      return statForType("attack")?.toString() ?: "-"
-    }
-
   val defenseValue: String
-    get() {
-      return statForType("defense")?.toString() ?: "-"
-    }
-
   val specialAttackValue: String
-    get() {
-      return statForType("special-attack")?.toString() ?: "-"
-    }
-
   val specialDefenseValue: String
-    get() {
-      return statForType("special-defense")?.toString() ?: "-"
-    }
-
   val speedValue: String
-    get() {
-      return statForType("speed")?.toString() ?: "-"
-    }
-
   val hpValue: String
-    get() {
-      return statForType("hp")?.toString() ?: "-"
+  val imageURL: String
+  val title: String = pokemon.name.capitalize()
+
+  init {
+    fun statForType(type: String): Int? {
+      return pokemon.stats.firstOrNull { stat ->
+        stat.stat.name == type
+      }
+          ?.baseStat
     }
 
-  private fun statForType(type: String): Int? {
-    return pokemon.stats.firstOrNull { stat ->
-      stat.stat.name == type
-    }?.baseStat
+    fun stringForStat(name: String): String {
+      return statForType(name)?.toString() ?: "-"
+    }
+
+    attackValue = stringForStat("attack")
+    defenseValue = stringForStat("defense")
+    specialAttackValue = stringForStat("special-attack")
+    specialDefenseValue = stringForStat("special-defense")
+    speedValue = stringForStat("speed")
+    hpValue = stringForStat("hp")
+
+    val formattedID = String.format("%03d", pokemon.id)
+    imageURL = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/$formattedID.png"
   }
 }
